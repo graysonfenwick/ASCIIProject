4ad099e892b9a029ab7555817ce24c059f3d27c8
graysonfenwick <graysonfenwick@gmail.com>
2017-10-02 17:42:25 -0400

    Updated

--- Project.java
+++ Project.java
@@ -1,7 +1,7 @@
 public class Project {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("I hope this works");
+        System.out.println("Wesome");
     }
 
 }
